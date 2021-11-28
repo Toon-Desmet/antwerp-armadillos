02ac74292cd5878341c44db7800ea6ad9aa5b42f
whitestike <32246781+whitestike@users.noreply.github.com>
2021-11-28 12:55:10 +0100

    header and footer done

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,2 +1,19 @@
+    <footer class="footer">
+        <div class="footer--element">
+            <p>Copyright © 2021 Antwerp Armadillos Lacrosse | Credits</p>
+        </div>
+        <div class="footer--element footer--link-element">
+            <h3>CONTACT US:</h3>
+            <p>antwerparmadillos@gmail.com</p>
+            <div>
+                <a href="https://www.facebook.com/antwerparmadillos/" target="_blank"><img src="assets/images/Facebook.svg" alt="link to facebook"></a>
+                <a href="https://www.instagram.com/antwerparmadillos/" target="_blank"><img src="assets/images/instagram.svg" alt="link to instagram"></a>
+            </div>
+        </div>
+        <div class="footer--element footer--design-element">
+            <p>Webdesign by</p>
+            <img src="assets/images/SAM.png" alt="logo of SAM"> 
+        </div>
+    </footer>
 </body>
 </html>
--- site/snippets/header.php
+++ site/snippets/header.php
@@ -5,10 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?=$page->Title()?></title>
+    <?= css('assets/css/index.css')?>
 </head>
 <body>
     <header>
-        <?php foreach ($site->children()->listed() as $links): ?>
-            <a href="<?php echo $links->url()?>"><?php echo $links->title() ?></a>
-        <?php endforeach?>
+        <a href="<?php echo $site->url()?>"><h1><?php echo $site->title()?><h1></a>
+
+        <div class="nav--links">
+            <?php foreach ($site->children()->listed() as $links): ?>
+                <a class="nav--links-link" href=<?php echo $links->url()?>><?php echo $links->title() ?></a>
+            <?php endforeach?>
+        </div>
     </header>
