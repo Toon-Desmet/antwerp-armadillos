14586257f0ec4a4d9d7c32b4f3e5e6db47dfb9cd
Toon <toon.desmet@student.kdg.be>
2021-12-14 15:02:21 +0100

    Added armadillos favicon

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,19 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?=$page->Title()?></title>
-    <?= css('assets/css/index.css')?>
+    <link rel="icon" type="image/jpg" href="/assets/images/Armadillos_logo_500x500.jpg" />
+    <title><?= $page->Title() ?> | Antwerp Armadillos</title>
+    <?= css('assets/css/index.css') ?>
 </head>
+
 <body>
     <header>
-        <a href="<?php echo $site->url()?>"><h1><?php echo $site->title()?><h1></a>
+        <a href="<?php echo $site->url() ?>">
+            <h1><?php echo $site->title() ?><h1>
+        </a>
 
         <div class="nav--links">
-            <?php foreach ($site->children()->listed() as $links): ?>
-                <a class="nav--links-link" href=<?php echo $links->url()?>><?php echo $links->title() ?></a>
-            <?php endforeach?>
+            <?php foreach ($site->children()->listed() as $links) : ?>
+                <a class="nav--links-link" href=<?php echo $links->url() ?>><?php echo $links->title() ?></a>
+            <?php endforeach ?>
         </div>
     </header>
