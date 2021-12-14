245fe6203d158397b394637078c15799a9062cb3
Toon <toon.desmet@student.kdg.be>
2021-12-14 14:37:55 +0100

    Added global social links

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -6,14 +6,15 @@
             <h3>CONTACT US:</h3>
             <p>antwerparmadillos@gmail.com</p>
             <div>
-                <a href="https://www.facebook.com/antwerparmadillos/" target="_blank"><img src="assets/images/Facebook.svg" alt="link to facebook"></a>
-                <a href="https://www.instagram.com/antwerparmadillos/" target="_blank"><img src="assets/images/instagram.svg" alt="link to instagram"></a>
+                <a href="<?= $site->facebook()->html() ?>" target="_blank"><img src="assets/images/Facebook.svg" alt="link to facebook"></a>
+                <a href="<?= $site->instagram()->html() ?>" target="_blank"><img src="assets/images/instagram.svg" alt="link to instagram"></a>
             </div>
         </div>
         <div class="footer--element footer--design-element">
             <p>Webdesign by</p>
-            <img src="assets/images/SAM.png" alt="logo of SAM"> 
+            <img src="assets/images/SAM.png" alt="logo of SAM">
         </div>
     </footer>
-</body>
-</html>
+    </body>
+
+    </html>
