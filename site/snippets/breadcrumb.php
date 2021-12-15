6c495a80d031e86a2fa35e943d6b4305a0b36792
Toon <toon.desmet@student.kdg.be>
2021-12-15 15:11:02 +0100

    Created a breadcrumb snippet

---
+++ site/snippets/breadcrumb.php
@@ -0,0 +1,12 @@
+<breadcrumb>
+    //TODO implement the last breadcrumb as the redirect
+    <button><a href="/home">back</a></button>
+    <!-- <?php foreach ($site->breadcrumb() as $key => $crumb) :
+                //TODO  Fixen naar de eerste key in plaats van home
+                if ($crumb->title() != "Home") {
+                    echo "/";
+                }
+            ?>
+        <a href="<?= $crumb->url() ?>"><?= $crumb->title() ?></a>
+    <?php endforeach; ?> -->
+</breadcrumb>
