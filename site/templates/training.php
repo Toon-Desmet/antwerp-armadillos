1050f908a48ad6b426c212f671ba53badf304e20
Toon <toon.desmet@student.kdg.be>
2021-12-15 17:34:23 +0100

    added main tag in training

--- site/templates/training.php
+++ site/templates/training.php
@@ -1,38 +1,39 @@
 <?php snippet('header') ?>
 <?php snippet('breadcrumb') ?>
+<main>
+    <training>
+        <h2>Onze trainingen gaan door op:</h2>
 
-<training>
-    <h2>Onze trainingen gaan door op:</h2>
+        <?php if ($page->jeugd()->isNotEmpty()) : ?>
+            <h3>Voor de jeugd:</h3>
+            <p><?= $page->jeugd() ?></p>
+        <?php endif ?>
+        <?php if ($page->mannen()->isNotEmpty()) : ?>
+            <h3>Voor de mannen:</h3>
+            <p><?= $page->mannen() ?></p>
+        <?php endif ?>
+        <?php if ($page->vrouwen()->isNotEmpty()) : ?>
+            <h3>Voor de vrouwen:</h3>
+            <p><?= $page->vrouwen() ?></p>
+        <?php endif ?>
+    </training>
 
-    <?php if ($page->jeugd()->isNotEmpty()) : ?>
-        <h3>Voor de jeugd:</h3>
-        <p><?= $page->jeugd() ?></p>
-    <?php endif ?>
-    <?php if ($page->mannen()->isNotEmpty()) : ?>
-        <h3>Voor de mannen:</h3>
-        <p><?= $page->mannen() ?></p>
-    <?php endif ?>
-    <?php if ($page->vrouwen()->isNotEmpty()) : ?>
-        <h3>Voor de vrouwen:</h3>
-        <p><?= $page->vrouwen() ?></p>
-    <?php endif ?>
-</training>
+    <interesse>
+        <h2>Geinteresseerd?</h2>
+        <?php if ($page->interesse()->isNotEmpty()) : ?>
+            <p><?= $page->interesse() ?></p>
+        <?php endif ?>
 
-<interesse>
-    <h2>Geinteresseerd?</h2>
-    <?php if ($page->interesse()->isNotEmpty()) : ?>
-        <p><?= $page->interesse() ?></p>
-    <?php endif ?>
+        <button><a href="/contact">Inschrijven</a></button>
+    </interesse>
 
-    <button><a href="/contact">Inschrijven</a></button>
-</interesse>
-
-<location>
-    <h2>Locatie</h2>
-    <?php if ($page->googlemap()->isNotEmpty()) : ?>
-        <?= $page->googlemap() ?>
-    <?php else : ?>
-        <?= $page->locatie() ?>
-    <?php endif ?>
-</location>
+    <location>
+        <h2>Locatie</h2>
+        <?php if ($page->googlemap()->isNotEmpty()) : ?>
+            <?= $page->googlemap() ?>
+        <?php else : ?>
+            <?= $page->locatie() ?>
+        <?php endif ?>
+    </location>
+</main>
 <?php snippet('footer') ?>
