15c504e114d519ae2aed37cda412b806820aa53b
Toon <toon.desmet@student.kdg.be>
2021-12-15 16:56:24 +0100

    Created a training template and added content

---
+++ site/templates/training.php
@@ -0,0 +1,38 @@
+<?php snippet('header') ?>
+<?php snippet('breadcrumb') ?>
+
+<training>
+    <h2>Onze trainingen gaan door op:</h2>
+
+    <?php if ($page->jeugd()->isNotEmpty()) : ?>
+        <h3>Voor de jeugd:</h3>
+        <p><?= $page->jeugd() ?></p>
+    <?php endif ?>
+    <?php if ($page->mannen()->isNotEmpty()) : ?>
+        <h3>Voor de mannen:</h3>
+        <p><?= $page->mannen() ?></p>
+    <?php endif ?>
+    <?php if ($page->vrouwen()->isNotEmpty()) : ?>
+        <h3>Voor de vrouwen:</h3>
+        <p><?= $page->vrouwen() ?></p>
+    <?php endif ?>
+</training>
+
+<interesse>
+    <h2>Geinteresseerd?</h2>
+    <?php if ($page->interesse()->isNotEmpty()) : ?>
+        <p><?= $page->interesse() ?></p>
+    <?php endif ?>
+
+    <button><a href="/contact">Inschrijven</a></button>
+</interesse>
+
+<location>
+    <h2>Locatie</h2>
+    <?php if ($page->googlemap()->isNotEmpty()) : ?>
+        <?= $page->googlemap() ?>
+    <?php else : ?>
+        <?= $page->locatie() ?>
+    <?php endif ?>
+</location>
+<?php snippet('footer') ?>
