043f187b1d555f96395b8dc8a1dc13c608bc61b0
Toon <toon.desmet@student.kdg.be>
2021-12-15 15:49:50 +0100

    Updated footer snippet so that socials images load on every page

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -6,8 +6,8 @@
             <h3>CONTACT US:</h3>
             <p>antwerparmadillos@gmail.com</p>
             <div>
-                <a href="<?= $site->facebook()->html() ?>" target="_blank"><img src="assets/images/Facebook.svg" alt="link to facebook"></a>
-                <a href="<?= $site->instagram()->html() ?>" target="_blank"><img src="assets/images/instagram.svg" alt="link to instagram"></a>
+                <a href="<?= $site->facebook()->html() ?>" target="_blank"><img src="<?= url('assets/images/Facebook.svg') ?>" alt="link to facebook"></a>
+                <a href="<?= $site->instagram()->html() ?>" target="_blank"><img src="<?= url('assets/images/instagram.svg') ?>" alt="link to instagram"></a>
             </div>
         </div>
         <div class="footer--element footer--design-element">
