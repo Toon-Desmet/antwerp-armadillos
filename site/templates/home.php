70501e12012a69cc98053ff634c00e83a0bfc672
Toon <toon.desmet@student.kdg.be>
2021-12-15 17:33:35 +0100

    replaced code with snippet

--- site/templates/home.php
+++ site/templates/home.php
@@ -2,22 +2,13 @@
 
 <main>
     <hero>
-        <h1><?= $page->teamtitle() ?></h1>
+        <h1><?= $site->teamtitle() ?></h1>
         <h2><?= $page->subtitle() ?></h2>
         <button><a href="/contact">Inschrijven</a></button>
     </hero>
 
-    <grid>
-        <?php foreach (page('news')->children()->listed() as $post) : ?>
-            <a href="<?= $post->url() ?>">
-                <figure>
-                    <img src="<?= $post->image()->crop(400)->url() ?> ">
-                    <figcaption> <?= $post->title() ?></figcaption>
-                    <a href="<?= $post->url() ?>">Lees meer</a>
-                </figure>
-            </a>
-        <?php endforeach ?>
-    </grid>
+    <?php snippet('news-grid') ?>
+
 
     <article>
         <p><?= $page->lacrosse_text() ?></p>
