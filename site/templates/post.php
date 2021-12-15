fd9d6d9a3fe0420d22cdc7e06449fad7f148cf88
Toon <toon.desmet@student.kdg.be>
2021-12-15 15:27:18 +0100

    Made a simple post template

---
+++ site/templates/post.php
@@ -0,0 +1,12 @@
+<?php snippet('header') ?>
+<?php snippet('breadcrumb') ?>
+
+<main>
+    <article>
+
+        <h1><?= $page->title() ?></h1>
+
+    </article>
+</main>
+
+<?php snippet('footer') ?>
