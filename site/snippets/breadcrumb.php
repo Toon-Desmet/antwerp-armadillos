16611adbeca6ebc325d0c1c5a846208e2767354d
Toon <toon.desmet@student.kdg.be>
2021-12-15 15:26:58 +0100

    commented todo in breadcrumb

--- site/snippets/breadcrumb.php
+++ site/snippets/breadcrumb.php
@@ -1,5 +1,6 @@
 <breadcrumb>
-    //TODO implement the last breadcrumb as the redirect
+    <!-- //TODO implement the last breadcrumb as the redirect -->
+    <!-- //TODO changing text button to arrow button -->
     <button><a href="/home">back</a></button>
     <?php foreach ($site->breadcrumb() as $key => $crumb) :
         //TODO  Fixen naar de eerste key in plaats van home
