13e62c89ea7ca612a6759f10f85ab4926bfaf84f
Toon <toon.desmet@student.kdg.be>
2021-12-19 17:21:17 +0100

    Created contact page

--- site/templates/contact.php
+++ site/templates/contact.php
@@ -2,8 +2,34 @@
 <?php snippet('breadcrumb') ?>
 
 <main>
-    <h1><?= $page->title()->html() ?></h1>
+    <h1><?= $page->custom_title()->or($page->title()->html()) ?></h1>
 
+    <form action="<?= $page->url() ?>">
+
+        <field>
+            <label for="name">Naam</label>
+            <input type="text" id="name" name="name" placeholder="Naam" required>
+        </field>
+
+        <field>
+            <label for="email">Email</label>
+            <input type="text" id="email" name="email" placeholder="Email" required>
+        </field>
+
+        <field>
+            <label for="text">Opmerking</label>
+            <textarea type="textarea" id="text" name="text" placeholder="Vraag of opmerking" required></textarea>
+        </field>
+
+        <input type="submit" name="submit" value="Submit">
+    </form>
+
+    <info>
+        <?= $page->lacrosse_info() ?><a href="<?= page('wat')->url() ?>"> klik hier</a>
+    </info>
+    <info>
+        <?= $page->training_info() ?><a href="<?= page('training')->url() ?>"> klik hier</a>
+    </info>
 </main>
 
 <?php snippet('footer') ?>
