5d3c5f97f506c35ed4f43b3b577cd166504097ae
Rickert <rickert.goyvaerts@gmail.com>
2021-12-27 11:42:09 +0100

    breadcrumbs and buttons

--- site/snippets/breadcrumb.php
+++ site/snippets/breadcrumb.php
@@ -1,11 +1,11 @@
 <breadcrumb>
     <!-- //TODO implement the last breadcrumb as the redirect -->
     <!-- //TODO changing text button to arrow button -->
-    <button><a href="/home">back</a></button>
+    <a class="breadcrumbs--back-arrow" href="<?php echo $site->url() ?>"><img src="./assets/images/back_arrow.svg" alt=""></a>
     <?php foreach ($site->breadcrumb() as $key => $crumb) :
         //TODO  Fixen naar de eerste key in plaats van home
         if ($crumb->title() != "Home") {
-            echo "/";
+            echo "<h4>/</h4>";
         }
     ?>
         <a href="<?= $crumb->url() ?>"><?= $crumb->title()->lower() ?></a>
