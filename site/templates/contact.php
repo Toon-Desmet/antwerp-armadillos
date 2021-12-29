1e6d58f271e5bd103669c62c2a552b0af5f1a3df
Rickert <rickert.goyvaerts@gmail.com>
2021-12-29 11:28:24 +0100

    contact page done

--- site/templates/contact.php
+++ site/templates/contact.php
@@ -2,34 +2,35 @@
 <?php snippet('breadcrumb') ?>
 <!-- //TODO update controller -->
 <main>
-    <h1><?= $page->custom_title()->or($page->title()->html()) ?></h1>
+    <h3><?= $page->custom_title()->or($page->title()->html()) ?></h3>
 
-    <form action="<?= $page->url() ?>">
+    <div class="container--contact-form">
+        <form class="form--contact" action="<?= $page->url() ?>">
 
-        <field>
-            <label for="name">Naam</label>
-            <input type="text" id="name" name="name" placeholder="Naam" required>
-        </field>
+            <field>
+                <input type="text" id="name" name="name" placeholder="Naam" required>
+            </field>
 
-        <field>
-            <label for="email">Email</label>
-            <input type="text" id="email" name="email" placeholder="Email" required>
-        </field>
+            <field>
+                <input type="text" id="email" name="email" placeholder="Email" required>
+            </field>
 
-        <field>
-            <label for="text">Opmerking</label>
-            <textarea type="textarea" id="text" name="text" placeholder="Vraag of opmerking" required></textarea>
-        </field>
+            <field>
+                <textarea type="textarea" id="text" name="text" placeholder="Vraag of opmerking" required></textarea>
+            </field>
 
-        <input class="button button--primary" type="submit" name="submit" value="Submit">
-    </form>
+            <input class="button button--primary" type="submit" name="submit" value="Submit">
+        </form>
+    </div>
 
-    <info>
-        <?= $page->lacrosse_info() ?><a href="<?= page('wat')->url() ?>"> klik hier</a>
-    </info>
-    <info>
-        <?= $page->training_info() ?><a href="<?= page('training')->url() ?>"> klik hier</a>
-    </info>
+    <div class="container--contact-info">
+        <info>
+            <?= $page->lacrosse_info() ?><a href="<?= page('wat')->url() ?>"> klik hier</a>
+        </info>
+        <info>
+            <?= $page->training_info() ?><a href="<?= page('training')->url() ?>"> klik hier</a>
+        </info>
+    </div>
 </main>
 
 <?php snippet('footer') ?>
