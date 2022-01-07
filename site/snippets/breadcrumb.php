8c8a735c61c2398811c8a66fc47206a32136d7ec
Toon <toon.desmet@student.kdg.be>
2022-01-07 16:01:46 +0100

    back arrow now points to parent page

--- site/snippets/breadcrumb.php
+++ site/snippets/breadcrumb.php
@@ -1,9 +1,17 @@
 <breadcrumb>
-    <!-- //TODO implement the last breadcrumb as the redirect -->
-    <!-- //TODO changing text button to arrow button -->
-    <a class="breadcrumbs--back-arrow" href="<?php echo $site->url() ?>"><img src="./assets/images/back_arrow.svg" alt=""></a>
+    <?php
+    $backUrl;
+    if ($page->parent()) {
+        $backUrl = $page->parent()->url();
+    } else {
+        $backUrl = $site->url();
+    }
+    ?>
+    <a class="breadcrumbs--back-arrow" href="<?= $backUrl ?>">
+        <img src="<?= asset("assets/images/back_arrow.svg")->url() ?>" alt="back arrow">
+    </a>
+
     <?php foreach ($site->breadcrumb() as $key => $crumb) :
-        //TODO  Fixen naar de eerste key in plaats van home
         if ($crumb->title() != "Home") {
             echo "<h4>/</h4>";
         }
