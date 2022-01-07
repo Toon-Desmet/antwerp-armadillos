2c02dbdf3906f8cde116a4af36d40c0dc809a9f9
Toon <toon.desmet@student.kdg.be>
2022-01-07 23:11:37 +0100

    added config for mailhog testing

--- site/config/config.php
+++ site/config/config.php
@@ -2,4 +2,12 @@
 
 return [
     'debug'  => true,
+    'email' => [
+        'transport' => [
+            'type' => 'smtp',
+            'host' => 'mailserver.antwerparmadillos.internal',
+            'port' => 1025,
+            'security' => false
+        ]
+    ],
 ];
